707c059771e996a4054498d260722badb66444b6
slemeshaev <lemeshaevs@gmail.com>
2026-05-04 19:16:39 +0300

    Implement command-line interface structure for xkcd comic retrieval and search
    - Add Comic struct with fields for comic metadata (num, date, title, transcript, alt, img)
    - Define usage string with three subcommands: get, index, search
    - Add usageDie helper function to display usage and exit
    - Implement main command dispatcher with switch on subcommand
    - Validate minimum arguments before dispatching

---
+++ chapter4/ex4_12/xkcd.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type Comic struct {
+	Num              int
+	Year, Month, Day string
+	Title            string
+	Transcript       string
+	Alt              string
+	Img              string
+}
+
+const usage = `xkcd get N
+xkcd index OUTPUT_FILE
+xkcd search INDEX_FILE QUERY`
+
+func usageDie() {
+	fmt.Println(usage)
+	os.Exit(1)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, usage)
+		os.Exit(1)
+	}
+	cmd := os.Args[1]
+	switch cmd {
+	case "get":
+		// Implementation
+	case "index":
+		// Implementation
+	case "search":
+		// Implementation
+	default:
+		usageDie()
+	}
+}
