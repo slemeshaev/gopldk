35a10503aeb59d394f93b0071c0807449012b516
slemeshaev <lemeshaevs@gmail.com>
2026-05-05 19:26:31 +0300

    Add functionality to fetch a single comic by number from xkcd API
    - Implement getComic function that fetches comic data from https://xkcd.com/{id}/info.0.json
    - Parse JSON response into Comic struct using json.NewDecoder
    - Add error handling for HTTP errors and non-200 status codes
    - Add command-line argument validation for "get" subcommand
    - Convert string argument to int with strconv.Atoi validation
    - Display comic data after successful fetch

--- chapter4/ex4_12/xkcd.go
+++ chapter4/ex4_12/xkcd.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"strconv"
 )
 
 type Comic struct {
@@ -32,7 +36,22 @@ func main() {
 	cmd := os.Args[1]
 	switch cmd {
 	case "get":
-		// Implementation
+		if len(os.Args) != 3 {
+			usageDie()
+		}
+
+		n, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "N (%s) must be an in", os.Args[1])
+			usageDie()
+		}
+
+		comic, err := getComic(n)
+		if err != nil {
+			log.Fatal("Error getting comic", err)
+		}
+
+		fmt.Println(comic)
 	case "index":
 		// Implementation
 	case "search":
@@ -40,4 +59,26 @@ func main() {
 	default:
 		usageDie()
 	}
+}
+
+func getComic(n int) (Comic, error) {
+	var comic Comic
+	url := fmt.Sprintf("https://xkcd.com/%d/info.0.json", n)
+	fmt.Println(url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return comic, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return comic, fmt.Errorf("can't get comic %d: %s", n, resp.Status)
+	}
+
+	if err = json.NewDecoder(resp.Body).Decode(&comic); err != nil {
+		return comic, err
+	}
+
+	return comic, nil
 }
