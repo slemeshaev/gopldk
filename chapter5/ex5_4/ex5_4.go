c0c4f612662434ca39e9c880ad4a2ecf3fe62cb8
slemeshaev <lemeshaevs@gmail.com>
2026-05-22 19:58:00 +0300

    Extract additional links from HTML
    - Extend visit function to handle more element types
    - Add support for <link> tags (href attributes)
    - Add support for <img> and <script> tags (src attributes)
    - Keep existing <a> tag handling (href attributes)
    - Recursively traverse HTML node tree using FirstChild/NextSibling

---
+++ chapter5/ex5_4/ex5_4.go
@@ -0,0 +1,49 @@
+// Exercise 5.4: Extend the visit function so that it extracts other kinds of links from the document,
+// such as images, scripts, and style sheets.
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+func visit(links []string, n *html.Node) []string {
+	if n == nil {
+		return links
+	}
+
+	if n.Type == html.ElementNode {
+		switch n.Data {
+		case "a", "link":
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					links = append(links, a.Val)
+				}
+			}
+		case "img", "script":
+			for _, a := range n.Attr {
+				if a.Key == "src" {
+					links = append(links, a.Val)
+				}
+			}
+		}
+	}
+
+	links = visit(links, n.FirstChild)
+	return visit(links, n.NextSibling)
+}
