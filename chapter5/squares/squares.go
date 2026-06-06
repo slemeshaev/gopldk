77134cfa08be44ccf67ea3b8df28f41d3794b45c
slemeshaev <lemeshaevs@gmail.com>
2026-06-06 21:36:09 +0300

    Demonstrate closure with stateful function generator
    - Add squares function that returns a closure capturing x variable
    - Each call increments x and returns its square
    - Main demonstrates sequential calls producing 1, 4, 9, 16

---
+++ chapter5/squares/squares.go
@@ -0,0 +1,21 @@
+// squares returns a function that returns
+// the text square number each time it is called.
+package main
+
+import "fmt"
+
+func main() {
+	f := squares()
+	fmt.Println(f()) // "1"
+	fmt.Println(f()) // "4"
+	fmt.Println(f()) // "9"
+	fmt.Println(f()) // "16"
+}
+
+func squares() func() int {
+	var x int
+	return func() int {
+		x++
+		return x * x
+	}
+}
