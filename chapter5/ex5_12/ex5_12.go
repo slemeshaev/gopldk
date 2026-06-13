3a69d509b28028c9e623039dea0b1697b375a302
slemeshaev <lemeshaevs@gmail.com>
2026-06-13 10:42:21 +0300

    Replace global depth with closure variable
    - Remove package-level depth variable
    - Move depth inside outline function as local variable
    - Convert startElement/endElement into anonymous functions
    - Use closure to share depth between pre and post functions
    - Each outline call now has its own depth state

---
+++ chapter5/ex5_12/ex5_12.go
@@ -0,0 +1,61 @@
+// Exercise 5.12: The startElement and endElement functions in chapter5/outline2
+// share a global variable, depth. Turn them into anonymous functions that
+// share a variable local to the outline function.
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		outline(url)
+	}
+}
+
+func outline(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	var depth int
+	forEachNode(doc,
+		func(n *html.Node) {
+			if n.Type == html.ElementNode {
+				fmt.Fprintf(os.Stdout, "%*s<%s>\n", depth*2, "", n.Data)
+				depth++
+			}
+		},
+		func(n *html.Node) {
+			if n.Type == html.ElementNode {
+				depth--
+				fmt.Fprintf(os.Stdout, "%*s</%s>\n", depth*2, "", n.Data)
+			}
+		})
+	return nil
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
---
+++ chapter5/ex5_12/go.mod
@@ -0,0 +1,5 @@
+module github.com/slemeshaev/chapter5/ex5_12
+
+go 1.25.0
+
+require golang.org/x/net v0.56.0
