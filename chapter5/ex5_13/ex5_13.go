4692c0c85f92f2cd9b532dffaa019ea120d0d269
slemeshaev <lemeshaevs@gmail.com>
2026-06-15 10:38:25 +0300

    Implement domain-restricted page saving
    - Add savePage function to store HTML pages locally
    - Skip saving pages from different domains
    - Create directory structure based on URL host and path
    - Save pages as index.html for directory paths
    - Use original filename for pages with extensions
    - Copy response body directly to file with io.Copy
    - Handle file creation and closing with proper error propagation
    - Add error logging in crawl without stopping traversal

--- chapter5/ex5_13/ex5_13.go
+++ chapter5/ex5_13/ex5_13.go
@@ -5,8 +5,13 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
 )
 
 func breadthFirst(f func(item, domain string) []string, worklist []string) {
@@ -37,13 +42,63 @@ func breadthFirst(f func(item, domain string) []string, worklist []string) {
 }
 
 func crawl(url, domain string) []string {
-	// Implementation
-	return []string{}
+	fmt.Println(url)
+
+	err := savePage(url, domain)
+	if err != nil {
+		log.Printf("Can't save URL \"%s\": %s", url, err)
+	}
+
+	list, err := Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+
+	return list
 }
 
 func savePage(rawurl, domain string) error {
-	// Implementation
-	return nil
+	url, err := url.Parse(rawurl)
+	if err != nil {
+		return fmt.Errorf("bad url: %s", err)
+	}
+
+	if domain != url.Host {
+		return nil
+	}
+
+	dir := url.Host
+	var filename string
+	if filepath.Ext(url.Path) == "" {
+		dir = filepath.Join(dir, url.Path)
+		filename = filepath.Join(dir, "index.html")
+	} else {
+		dir = filepath.Join(dir, filepath.Dir(url.Path))
+		filename = url.Path
+	}
+
+	err = os.MkdirAll(dir, 0777)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Get(rawurl)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(file, resp.Body)
+	if closeErr := file.Close(); err == nil {
+		err = closeErr
+	}
+
+	return err
 }
 
 func main() {
