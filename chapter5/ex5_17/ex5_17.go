0902c21996b18a570b75f2c89567e81897f5d055
slemeshaev <lemeshaevs@gmail.com>
2026-06-22 07:49:59 +0300

    Add HTML parsing and demo for ElementsByTagName
    - Implement main function to open and parse page.html file
    - Use ElementsByTagName to extract all images and headings
    - Display image src attributes and heading text content
    - Add getNodeText helper to extract text from HTML nodes

--- chapter5/ex5_17/ex5_17.go
+++ chapter5/ex5_17/ex5_17.go
@@ -7,11 +7,50 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"golang.org/x/net/html"
 )
 
 func main() {
-	// Implementation
+	file, err := os.Open("page.html")
+	if err != nil {
+		panic(fmt.Sprintf("failed to open file: %v", err))
+	}
+	defer file.Close()
+
+	doc, err := html.Parse(file)
+	if err != nil {
+		panic(fmt.Sprintf("HTML parsing error: %v", err))
+	}
+
+	images := ElementsByTagName(doc, "img")
+	headings := ElementsByTagName(doc, "h1", "h2", "h3", "h4")
+
+	fmt.Println("Images:")
+	for i, node := range images {
+		for _, attr := range node.Attr {
+			if attr.Key == "src" {
+				fmt.Printf("  [%d] src=%s\n", i, attr.Val)
+			}
+		}
+	}
+
+	fmt.Println("\nHeadings:")
+	for i, node := range headings {
+		fmt.Printf("  [%d] tag=%s, text=%q\n", i, node.Data, getNodeText(node))
+	}
+}
+
+func getNodeText(n *html.Node) string {
+	var text string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.TextNode {
+			text += c.Data
+		}
+	}
+	return text
 }
 
 // ElementsByTagName that given an HTML node tree and zero or moe names, returns all the elements that match one of those names
