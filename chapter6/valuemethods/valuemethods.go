4eb8eff6eeb8d4fb45d6f09d37311484c2041de2
slemeshaev <lemeshaevs@gmail.com>
2026-07-13 19:33:12 +0300

    Demonstrate method values in Go
    - Assign p.Distance to method value distanceFromP
    - Call distanceFromP with different Point arguments
    - Assign p.ScaleBy to method value scaleP
    - Call scaleP multiple times to modify p incrementally

---
+++ chapter6/valuemethods/valuemethods.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	X, Y float64
+}
+
+// traditional function
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+// same thing, but as method of the Point type
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func (p *Point) ScaleBy(factor float64) {
+	p.X *= factor
+	p.Y *= factor
+}
+
+func main() {
+	p := Point{1, 2}
+	q := Point{4, 6}
+
+	distanceFromP := p.Distance        // method value
+	fmt.Println(distanceFromP(q))      // "5"
+	var origin Point                   // {0, 0}
+	fmt.Println(distanceFromP(origin)) // "2.23606797749979", sqrt(5)
+
+	scaleP := p.ScaleBy // method value
+	scaleP(2)           // p becomes (2, 4)
+	scaleP(3)           // then (6, 12)
+	scaleP(10)          // then (60, 120)
+}
