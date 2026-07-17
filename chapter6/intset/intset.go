0764372368780f7bae1cfd87411c0d911bc9a0c7
slemeshaev <lemeshaevs@gmail.com>
2026-07-17 06:13:23 +0300

    Implement UnionWith for IntSet
    - Add UnionWith method that performs bitwise OR on words
    - Handle cases where s has fewer words than t (append missing words)
    - Modify s in place with union of s and t
    - Add example usage in main demonstrating union

--- chapter6/intset/intset.go
+++ chapter6/intset/intset.go
@@ -25,6 +25,17 @@ func (s *IntSet) Has(x int) bool {
 	return word < len(s.words) && s.words[word]&(1<<bit) != 0
 }
 
+// UnionWith sets s to the union of s and t.
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
 func main() {
 	var set IntSet
 
@@ -32,4 +43,17 @@ func main() {
 
 	fmt.Println(set.Has(64))  // true
 	fmt.Println(set.Has(200)) // false
+
+	var s1 IntSet
+	s1.Add(1)
+	s1.Add(3)
+	fmt.Println("s1 = ", s1)
+
+	var s2 IntSet
+	s2.Add(2)
+	s2.Add(4)
+	fmt.Println("s2 = ", s2)
+
+	s1.UnionWith(&s2)
+	fmt.Println("After union s1 =", s1)
 }
