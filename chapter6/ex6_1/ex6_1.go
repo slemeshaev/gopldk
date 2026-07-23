bd9fc2a92b9ac232b1d4c84d52f655e989181ee6
slemeshaev <lemeshaevs@gmail.com>
2026-07-23 06:02:12 +0300

    Add method descriptions for IntSet exercise 6.1
    - Add comments for Len, Remove, Clear, and Copy methods

--- chapter6/ex6_1/ex6_1.go
+++ chapter6/ex6_1/ex6_1.go
@@ -1,3 +1,9 @@
+// Exercise 6.1: Implement these additional methods:
+// func (*IntSet) Len() int // return the number of elements
+// func (*IntSet) Remove(x int) // remove x from the set
+// func (*IntSet) Clear() // remove all elements from the set
+// func (*IntSet) Copy() *IntSet // return a copy of the set
+
 package main
 
 import (
