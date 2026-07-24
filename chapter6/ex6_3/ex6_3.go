5158fa223085795792ae3adb15fee9ae6219a109
slemeshaev <lemeshaevs@gmail.com>
2026-07-24 08:14:05 +0300

    Import all from Exercise 6.2

---
+++ chapter6/ex6_3/ex6_3.go
@@ -0,0 +1,11 @@
+// Exercise 6.3: (*IntSet).UnionWith computes the union of two sets using |,
+// the word-parallel bitwise OR operator. Implement methods for IntersectWith,
+// DifferenceWith, and SymmetricDifference for the corresponding set operations.
+// (The symmertric difference of two sets contains the elements present in one set or
+// the other but not both.)
+
+package main
+
+func main() {
+	//
+}
