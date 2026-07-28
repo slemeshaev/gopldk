b13a326a647233d431fa97fe40c274ed7c204cc1
slemeshaev <lemeshaevs@gmail.com>
2026-07-28 06:23:41 +0300

    Add Elems method for IntSet iteration
    - Implement Elems to return slice of all elements in the set
    - Iterate over each word and bit to collect values
    - Return slice suitable for range loop iteration
    - Add example usage in main

---
+++ chapter6/ex6_4/ex6_4.go
@@ -0,0 +1,185 @@
+// Exercise 6.4: Add a method Elems that returns a slice containing the elements of the set,
+// suitable for iterating over with a range loop.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// An IntSet is a set of small non-negative integers.
+// Its zero value represents the empty set.
+type IntSet struct {
+	words []uint64
+}
+
+// Add adds the nonnegative value x to the set.
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+
+	s.words[word] |= 1 << bit
+}
+
+// Has reports whether the set contains the non-negative value x.
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+// UnionWith sets s to the union of s and t.
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+// IntersectWith sets s to the intersection of s and t.
+func (s *IntSet) IntersectWith(t *IntSet) {
+	if len(s.words) > len(t.words) {
+		s.words = s.words[:len(t.words)]
+	}
+
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] &= tword
+		}
+	}
+}
+
+// DiffrenceWith sets s to the difference of s and t.
+func (s *IntSet) DifferenceWith(t *IntSet) {
+	if len(s.words) > len(t.words) {
+		s.words = s.words[:len(t.words)]
+	}
+
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] &= ^tword
+		}
+	}
+
+	for len(s.words) > 0 && s.words[len(s.words)-1] == 0 {
+		s.words = s.words[:len(s.words)-1]
+	}
+}
+
+// SymmetricDifference sets s to the symmetric difference of s and t.
+func (s *IntSet) SymmetricDifference(t *IntSet) {
+	if len(t.words) > len(s.words) {
+		s.words = append(s.words, make([]uint64, len(t.words)-len(s.words))...)
+	}
+
+	for i, tword := range t.words {
+		s.words[i] ^= tword
+	}
+
+	for len(s.words) > 0 && s.words[len(s.words)-1] == 0 {
+		s.words = s.words[:len(s.words)-1]
+	}
+}
+
+func bitCount(x uint64) int {
+	x = x - ((x >> 1) & 0x5555555555555555)
+	x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333)
+	x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f
+	x = x + (x >> 8)
+	x = x + (x >> 16)
+	x = x + (x >> 32)
+	return int(x & 0x7f)
+}
+
+// Return the number of elements
+func (s *IntSet) Len() int {
+	l := 0
+	for _, word := range s.words {
+		l += bitCount(word)
+	}
+
+	return l
+}
+
+// Remove x from the set
+func (s *IntSet) Remove(x int) {
+	word, bit := x/64, uint(x%64)
+	if len(s.words) == 0 || word > len(s.words) {
+		return
+	}
+
+	s.words[word] &= ^(1 << bit)
+}
+
+// Remove all elements from the set
+func (s *IntSet) Clear() {
+	for i := range s.words {
+		s.words[i] = 0
+	}
+}
+
+// Return a copy of the set
+func (s *IntSet) Copy() *IntSet {
+	c := new(IntSet)
+	c.words = make([]uint64, len(s.words))
+	copy(c.words, s.words)
+	return c
+}
+
+// AddAll addes all the non-negative values to the set
+func (s *IntSet) AddAll(vals ...int) {
+	for _, v := range vals {
+		s.Add(v)
+	}
+}
+
+// Elems returns a slice containing the elements of the set
+func (s *IntSet) Elems() (elems []int) {
+	for i, word := range s.words {
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				elems = append(elems, i*64+j)
+			}
+		}
+	}
+	return
+}
+
+// String returns the set as a string of the form "{1 2 3}".
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+func main() {
+	x := new(IntSet)
+	x.AddAll(1, 144, 30)
+
+	x.Elems()
+
+	fmt.Println(x.String())
+}
