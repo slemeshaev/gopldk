4d0daa9744562c1c550aff29ef001c9ff85bd154
slemeshaev <lemeshaevs@gmail.com>
2026-08-07 07:25:26 +0300

    Add Tree struct for exercise 7.3
    - Add Tree struct with value and left/right pointers

---
+++ chapter7/ex7_3/ex7_3.go
@@ -0,0 +1,13 @@
+// Exercise 7.3: Write a String method for the *tree type in gopl.io/ch4/treesort (4.4)
+// that reveals the sequence of values in the tree.
+
+package main
+
+type Tree struct {
+	value       int
+	left, right *Tree
+}
+
+func main() {
+	//
+}
