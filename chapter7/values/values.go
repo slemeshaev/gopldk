aa0e5e73c314b0955eab88ae6366170ad9817903
slemeshaev <lemeshaevs@gmail.com>
2026-08-21 17:17:40 +0300

    Add fmt.Printf("%T\n", w) to show interface type
    - Add %T formatting to display dynamic type of interface w
    - Show type changes: <nil> → *os.File → *bytes.Buffer → <nil>
    - Complement reflect.TypeOf output with simpler %T format

--- chapter7/values/values.go
+++ chapter7/values/values.go
@@ -12,17 +12,21 @@ func main() {
 	var w io.Writer
 	fmt.Println("1. type: ", reflect.TypeOf(w))
 	fmt.Println("1. value: ", reflect.ValueOf(w))
+	fmt.Printf("%T\n", w)
 
 	w = os.Stdout
 	fmt.Println("2. type: ", reflect.TypeOf(w))
 	fmt.Println("2. value: ", reflect.ValueOf(w))
+	fmt.Printf("%T\n", w)
 
 	w = new(bytes.Buffer)
 	fmt.Println("3. type: ", reflect.TypeOf(w))
 	fmt.Println("3. value: ", reflect.ValueOf(w))
 	w.Write([]byte("hello"))
+	fmt.Printf("%T\n", w)
 
 	w = nil
 	fmt.Println("4. type: ", reflect.TypeOf(w))
 	fmt.Println("4. value: ", reflect.ValueOf(w))
+	fmt.Printf("%T\n", w)
 }
