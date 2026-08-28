994e374dd1d71fabffe0fc72126d41149368fc38
slemeshaev <lemeshaevs@gmail.com>
2026-08-28 17:33:04 +0300

    Add customSort type for flexible track sorting
    - Implement sort.Interface (Len, Less, Swap) on customSort struct
    - Store a custom less function to allow arbitrary comparison logic
    - Sort tracks by Title, then Year, then Length as tiebreakers
    - Use sort.Sort with customSort to order tracks and print the result

--- chapter7/sorting/sorting.go
+++ chapter7/sorting/sorting.go
@@ -72,10 +72,45 @@ func (x byArtist) Swap(i, j int) {
 	x[i], x[j] = x[j], x[i]
 }
 
+type customSort struct {
+	t    []*Track
+	less func(x, y *Track) bool
+}
+
+func (x customSort) Len() int {
+	return len(x.t)
+}
+
+func (x customSort) Less(i, j int) bool {
+	return x.less(x.t[i], x.t[j])
+}
+
+func (x customSort) Swap(i, j int) {
+	x.t[i], x.t[j] = x.t[j], x.t[i]
+}
+
 func main() {
 	sort.Sort(byYear(tracks))
 	printTracks(tracks)
 
 	sort.Sort(sort.Reverse(byArtist(tracks)))
+	printTracks(tracks)
+
+	sort.Sort(customSort{tracks, func(x, y *Track) bool {
+		if x.Title != y.Title {
+			return x.Title < y.Title
+		}
+
+		if x.Year != y.Year {
+			return x.Year < y.Year
+		}
+
+		if x.Length != y.Length {
+			return x.Length < y.Length
+		}
+
+		return false
+	}})
+
 	printTracks(tracks)
 }
