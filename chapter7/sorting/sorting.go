19a24a0fd040f4172f0db8000bb6e84db20d87b9
slemeshaev <lemeshaevs@gmail.com>
2026-08-29 15:23:24 +0300

    Demonstrate sorting and reverse sorting with sort package - Check initial slice order with sort.IntsAreSorted before any sorting - Sort values in ascending order using sort.Ints - Reverse the order with sort.Sort(sort.Reverse(sort.IntSlice(values))) - Print intermediate results to show state after each operation

--- chapter7/sorting/sorting.go
+++ chapter7/sorting/sorting.go
@@ -113,4 +113,15 @@ func main() {
 	}})
 
 	printTracks(tracks)
+
+	values := []int{3, 1, 4, 1}
+	fmt.Println(sort.IntsAreSorted(values)) // false
+
+	sort.Ints(values)
+	fmt.Println(values)                     // [1 1 3 4]
+	fmt.Println(sort.IntsAreSorted(values)) // true
+
+	sort.Sort(sort.Reverse(sort.IntSlice(values)))
+	fmt.Println(values)                     // [4 3 1 1]
+	fmt.Println(sort.IntsAreSorted(values)) // false
 }
